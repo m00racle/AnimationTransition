2c8511622a9b642c83383f578dfaef3e548b8709
mooracle <yanuar.prakosa@gmail.com>
2018-12-27 08:52:33 +0700

    create detail activity

--- app/src/main/java/com/mooracle/animationtransition/AlbumDetailActivity.java
+++ app/src/main/java/com/mooracle/animationtransition/AlbumDetailActivity.java
@@ -6,16 +6,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AlbumDetailActivity extends AppCompatActivity {
 
     public static final String ALBUM_ART_RESID_EXTRA = "ALBUM ART RES ID";
 
+    //view declaration
+    ImageView albumArtView;
+    ImageButton fab;
+    ViewGroup titlePanel, trackPanel, detailContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_album_detail);
 
+        //instantiate views
+        setContentView(R.layout.activity_album_detail);
+        albumArtView = findViewById(R.id.album_art);
+        fab = findViewById(R.id.fab);
+        titlePanel = findViewById(R.id.title_panel);
+        trackPanel = findViewById(R.id.track_panel);
+        detailContainer = findViewById(R.id.detail_container);
     }
 
 }
